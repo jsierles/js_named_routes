ff71cb6ff943788a721616f5efdbef8f16d0fba3
Galen O'Hanlon <gohanlon@gohanlon-mbp2.local>
2008-05-22 20:07:56 -0700

    removed "named_routes" from files included when :defaults is given to javascript_include_tag

--- lib/asset_tag_helper.rb
+++ lib/asset_tag_helper.rb
@@ -1,9 +1,6 @@
 module ActionView::Helpers::AssetTagHelper
   alias_method :old_javascript_include_tag, :javascript_include_tag
 
-  JAVASCRIPT_DEFAULT_SOURCES = ['named_routes']
-  @@javascript_default_sources = JAVASCRIPT_DEFAULT_SOURCES.dup
-
   def javascript_include_tag(*sources)
     if sources.delete :named_routes
       sources = sources.concat(
