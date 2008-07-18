297fcfc255f75ea574703c1b62d019ab09ea8169
Matt Ittigson <matt@kluster.com>
2008-07-18 16:56:34 -0400

    We can't add the routes in the dispatch preparation because in non-development mode (when cache_classes is on) the routes are initialized after the dispatch is prepared which blows away our new route.  This hooks into the back of route loading, so the dispatch preparation lifecycle is out of the equation.

--- init.rb
+++ init.rb
@@ -5,8 +5,13 @@ require 'dispatcher'
 
 ActionView::Helpers::AssetTagHelper.send(:include, JsNamedRoutes::AssetTagHelper)
 
-ActionController::Dispatcher.to_prepare :named_routes_route do
-  JsNamedRoutes::ControllerMethods.add_routes
+ActionController::Routing::RouteSet.class_eval do
+  def load_routes_with_named_routes!
+    load_routes_without_named_routes!
+    JsNamedRoutes::ControllerMethods.add_routes 
+  end
+
+  alias_method_chain :load_routes!, :named_routes
 end
 
 controller_path = File.join(File.dirname(__FILE__), 'controllers')
